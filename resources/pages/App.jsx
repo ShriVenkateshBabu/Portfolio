f575a1a9dba1e813310261e017d6251f097e2bff
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2026-02-13 10:18:10 +0530

    Updated HeroSection, Navbar, Education timeline with arrows and animations

---
+++ resources/pages/App.jsx
@@ -0,0 +1,11 @@
+import Router from '../Route/index'
+import React from 'react'
+const App = () => {
+    return (
+        <>
+            <Router />
+        </>
+    )
+}
+
+export default App
---
+++ resources/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import HeroSection from '../Components/HeroSection'
+import Education from '../Components/Education'
+const Home = () => {
+    return (
+        <div>
+            {/* navbar */}
+            <NavBar />
+            {/* Hero Section */}
+            <HeroSection />
+            {/* Education */}
+            <Education />
+        </div>
+    )
+}
+
+export default Home
